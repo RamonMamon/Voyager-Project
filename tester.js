149ee0d0d3b575494094df3a2d2ef0434031498a
RamonMamon <35195972+RamonMamon@users.noreply.github.com>
2018-08-16 07:34:22 +0800

    Update 1.0.2
    Implemented:
    -KYC section (2 out of 4 buttons working)

--- tester.js
+++ tester.js
@@ -14,10 +14,11 @@ const client_id = "ramon-test";
 const client_secret = "Oox5bUpUKKtG07lWfQWztJgWB3wazc5Su8Mz1l3tKBqTn45ohzemH1wA5ZiHrb8iDiM3Q5Yso8cazz5ue5e2Tw";
 const base64Auth = base64.encode(client_id + ":" + client_secret);
 
-
 const redirect_uri = "http://localhost/callback";
 var access_token;
 var refresh_token;
+
+var kycSubmissionID;
 
 //These are static directories for the webpage to access.
 app.use(express.static(__dirname));
@@ -27,19 +28,78 @@ app.use(bodyParser.json());
 /**
  * Sends the index.html file to the client.
  */
-app.get('/', sendIndex);
+app.get('/', function (req,res){
+    console.log('Request Type: ', req.method);
+    res.status(OK);
+    res.sendFile(path.join(__dirname,'./index.html'));
 
-app.get('/callback', retrieveCallbackInfo);
+});
+
+/**
+ * Retrieves an access token without the user having to reauthenticate himself.
+ */
+function refresh(){
+    if(refresh_token != null){
+        var params = "grant_type=refresh_token&refresh_token=" + refresh_token;
+        authenticate(refresh_token, params)
+    }
+}
+
+/**
+ * Authenticates the user and returns an access token.
+ * @param {*} code 
+ * @param {*} params 
+ */
+function authenticate(code, params){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://connect-sb-issuing.paymaya.com/token',false);
+
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + base64Auth);
+    
+    console.log("Using Code/Refresh Token: " + code);
+    
+    xhr.send(params);
+    return xhr;
+}
+
+app.get('/callback', function(req,res){
+    res.sendFile(path.join(__dirname,'./index.html'));
+
+    var code = req.query.code
+
+    var params ="grant_type=authorization_code&code=" + code + "&redirect_uri="+ redirect_uri;
+    var xhr = authenticate(code, params);
+    // Parses the response as a JSON file.
+    var response = JSON.parse(xhr.responseText);
+    var status = xhr.status;
+
+    console.log();
+    console.log("This is the response body:");
+    console.log(response);
+    console.log();
+    console.log("Status " + status);
+
+    //Returns the access token and refresh token for any requests related to the API.
+    if (status == 200 && response.access_token != null && response.refresh_token != null){
+        access_token = response.access_token;
+        refresh_token = response.refresh_token;
+        console.log("\nThe Access token and Refresh token has been successfully retrieved.");
+    }else{
+        console.log("\nThere was a problem retrieving the access token.");
+        console.log("Check response object for more details on the error.");
+    }
+});
 
 
 app.get('/user', function(req, res){
     // Make the /user call of the PayMaya API retrieving the information of the user.
-    makeRequest("GET","/profile", res, false, 'Profile_Request', null, false);
+    makeRequest("GET","/profile", res, 'Profile_Request', null, false);
 });
 
 app.get('/balance', function(req, res){
     // Make the /balance call and send the balance to the html file.
-    makeRequest("GET","/balance", res, true, 'Balance_Request', null, false)
+    makeRequest("GET","/balance", res, 'Balance_Request', null, false)
 });
 
 app.post('/transfer', function(req,res){
@@ -62,25 +122,27 @@ app.post('/transfer', function(req,res){
         "note": note
         }
 
-    makeRequest('POST','/transfer', res, true, 'Transfer_Request', body, false);
+    makeRequest('POST','/transfer', res, 'Transfer_Request', body, false);
 });
 
+/**
+ * This makes a request to the PayMaya external API that confirms either an existing transfer
+ * or an existing payment.
+ */
 app.put('/confirm', function(req,res){
-    
     var type = req.body.type;
     var request;
     if(type == 'transfer'){
         var alias = req.body.alias;
         console.log('Confirming transfer request with the ID: ' + alias);
         request = '/transfer/' + alias + '/execute';
-        makeRequest('PUT',request, res, false, 'Transfer_Request', null, false);
+        makeRequest('PUT',request, res, 'Transfer_Request', null, false);
     }else if(type == 'payment'){
-        //TODO: TEST
         // Executes a bill payment.
         var billerID = req.body.billerID;
         console.log('Confirming payment to the Biller ID: ' + billerID);
         request = '/billpayment/' + billerID + '/execute';
-        makeRequest('POST', request,res,true,'Payment_Request_Confirmation', null, false);
+        makeRequest('POST', request,res,'Payment_Request_Confirmation', null, false);
     }
 });
 
@@ -90,41 +152,74 @@ app.put('/confirm', function(req,res){
 app.post('/cancel', function(req,res){
     var alias = req.body.alias;
     var request = '/transfer/' + alias;        
-    makeRequest('DELETE',request, res, false, 'Transfer_Request', null, false);
+    makeRequest('DELETE',request, res, 'Transfer_Request', null, false);
 });
 
 app.get('/productCatalog', function(req,res){
-    makeRequest('GET','/shop/products', res, false, 'Retrieve_Catalog', null, false);
+    makeRequest('GET','/shop/products', res, 'Retrieve_Catalog', null, false);
 });
 
 app.post('/purchase', function(req,res){
-    //TODO: Test this.
     var body = {
-        "purchaseId": req.body.purchaseId,
-        "productId": req.body.productId
-        }
-    makeRequest('POST','/shop/purchase', res, true, 'Retrieve_Catalog', body, false);
+        "purchaseId": req.body.purchaseID,
+        "productId": req.body.productID
+    }
+    makeRequest('POST','/shop/purchase', res, 'Retrieve_Catalog', body, false);
 
 });
 
 // Requests and sends the list of all the billers.
 app.get('/billers', function(req,res){
-    makeRequest('GET','/billers', res, true, 'Get_Billers', null, true);
+    makeRequest('GET','/billers', res, 'Get_Billers', null, true);
 });
 
 // Makes a P100 payment to a biller.
 app.post('/payBill', function(req,res){
-    //TODO: Update the body object and make sure it works.
     var billerID = req.body.billerID
+    console.log(billerID)
     var body =  {
         "biller": billerID,
         "amount": {
             "currency": "PHP",
             "value": 100,
+        },
+        "fields": {
+            "DueDate": "2016-07-10",
+            "AccountNumber": "30099492"
+        },
+        "notify": {
+            "url": "https://example.com/notifyBill",
+            "meta": {
+                "id":"bill123",
+                "special_token": "abcdefghi"
+            }
         }
     }
-    makeRequest('POST','/billpayment', res, true, 'Init_billpayment', body, false);
+    makeRequest('POST','/billpayment', res, 'Init_billpayment', body, false);
 });
+
+app.get('/getFields',function(req,res){
+    makeRequest('GET','/kyc/fields?profile=KYC1', res, 'Get_Fields', null, true);
+});
+
+app.get('/uploadFile',function(req,res){
+    var body = {
+
+    }
+    makeRequest('POST','/kyc/upload', res, 'Upload_Docs', body, false);
+});
+
+app.get('/submit', function(req,res){
+    var body = {
+
+    }
+    makeRequest('POST','/kyc/KYC1', res, 'Submit_Details', body, false);
+})
+
+app.get('/status', function(req,res){
+    var request = "kyc/status/" + kycSubmissionID;
+    makeRequest('GET',request, res, 'Get_Status', null, false);
+})
 
 /**
  * This function will be used to make any request to the PayMaya Sandbox API. 
@@ -135,17 +230,17 @@ app.post('/payBill', function(req,res){
  * @param {String} requestType Is the type of request made to the url.
  * @param {String} endpoint Is the destination for the request of a given URL.
  * @param {Object} res Is the response object.
- * @param {boolean} isJson Determines whether to include a json content type header.
  * @param {String} requestNo Will be used to verify and retrieve request information by PayMaya
  * @param {Object} body Is the Request body.
  * @param {boolean} basicAuth Determines whether or not to use basic-auth authorization. If false, an access token will be used instead.
  */
-function makeRequest(requestType, endpoint, res, isJson, requestNo, body, basicAuth){
+function makeRequest(requestType, endpoint, res, requestNo, body, basicAuth){
     // Checks if the User is authenticated with an access token.
     if(access_token != null){
+        refresh();
         var request = new XMLHttpRequest();
         request.open(requestType, "https://api-test.paymaya.com/external-api-sandbox/v1/" + endpoint,false);
-        if(isJson){
+        if(body!= null){
             request.setRequestHeader('Content-Type', 'application/json');
         }
 
@@ -165,74 +260,14 @@ function makeRequest(requestType, endpoint, res, isJson, requestNo, body, basicA
         console.log("\nRequest Status returned as " + request.status);
         if(request.status == 200 || request.status == 201){
             console.log("Access to endpoint " + endpoint + " was successful.");
+        }else if (request.status == 204){
+            console.log("The request was successful, but returned with an empty response.");
         }else{
             console.log("An error has occurred.")
             console.log("Please check the returned response on the client.");
         }
     }else{
         missingAccTok(res);
-    }
-}
-
-/**
- * Create an error response similar to the makeRequest()
- */
-
-/**
- * Create my own way to track the transaction history of an account.
- */
-
- /**
-  * Sends the Default index page.
-  */
-function sendIndex(req,res){
-    console.log('Request Type: ', req.method);
-    res.status(OK);
-    res.sendFile(path.join(__dirname,'./index.html'));
-
-}
-
-/**
- * Retrieves the required tokens and information from the callback page.
- * 
- * @param {Object} req 
- * @param {Object} res 
- */
-function retrieveCallbackInfo(req,res){
-    res.sendFile(path.join(__dirname,'./index.html'));
-    let code = req.query.code;
-    console.log("The Authentication Code is: " + code);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://connect-sb-issuing.paymaya.com/token',false);
-
-    // Basic Authentication. (Not stated in the documentation)
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + base64Auth);
-    
-    // This is the request body in the x-www-form-urlencoded format.
-    var params = "grant_type=authorization_code&code=" + code + "&redirect_uri="+ redirect_uri;
-    
-    xhr.send(params);
-
-    // Parses the response as a JSON file.
-    var response = JSON.parse(xhr.responseText);
-    var status = xhr.status;
-
-    console.log();
-    console.log("This is the response body:");
-    console.log(response);
-    console.log();
-    console.log("Status " + status);
-
-    //Returns the access token and refresh token for any requests related to the API.
-    if (status == 200 && response.access_token != null && response.refresh_token != null){
-        access_token = response.access_token;
-        refresh_token = response.refresh_token;
-        console.log("\nThe Access token has been successfully retrieved.");
-    }else{
-        console.log("\nThere was a problem retrieving the access token.");
-        console.log("Check response object for more details on the error.");
     }
 }
 
